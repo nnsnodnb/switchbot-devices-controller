546670a7ec827d6693503c502573da23965357c4
nnsnodnb <nnsnodnb@gmail.com>
2025-02-27 12:51:47 +0900

    Add zipper.py for zipped this project

---
+++ misc/zipper.py
@@ -0,0 +1,44 @@
+import zipfile
+from collections.abc import Generator
+from pathlib import Path
+
+
+IGNORE_PATHS = [
+    ".pytest_cache",
+    ".ruff_cache",
+    "tests",
+    "__pycache__",
+    ".venv",
+    ".git",
+    "cloudformation",
+    ".idea",
+    ".env",
+    ".DS_Store",
+]
+
+
+def _walk(src: Path) -> Generator[Path, None, None]:
+    for item in src.iterdir():
+        if item.name in IGNORE_PATHS:
+            continue
+
+        if item.is_dir():
+            yield from _walk(item)
+        else:
+            yield item
+
+
+def zipped(src: Path, dest: Path) -> Path:
+    dest.mkdir(exist_ok=True)
+    dist = dest / "switchbot-devices-controller.zip"
+    if dist.exists():
+        dist.unlink()
+
+    with zipfile.ZipFile(dist, "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
+        for target in _walk(src=src):
+            if target == dist or target == dest:
+                continue
+            filename = target.relative_to(src)
+            zf.write(filename=target, arcname=filename)
+
+    return dist
